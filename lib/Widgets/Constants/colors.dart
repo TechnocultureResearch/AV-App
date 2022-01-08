8d309947ba22e681a7a5d4dd955ee6080bc7c159
rosha <roshanmathewsofficial@gmail.com>
2022-01-08 17:14:33 +0530

    feat: add colors

---
+++ lib/Widgets/Constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const PrimaryGrey = Color(0xFFE5E5E5);
+const ButtonColor = Color(0xFF393229);
+const TextFieldThemeColor = Colors.black45;
+const ContainerWhite = Color(0xFFFFFFFF);
+const NavBarIconColor = Colors.grey;
+const SelectedIconColor = Colors.blue;
