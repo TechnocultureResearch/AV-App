553c941e96430a4d8a0d7242b769bfbaca740d45
rosha <roshanmathewsofficial@gmail.com>
2022-01-11 22:14:10 +0530

    feat: add review barchart

---
+++ lib/Screens/Satt UI/Dashborad page/bar_chart.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class _BarChart extends StatelessWidget {
+  const _BarChart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 20,
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+    enabled: false,
+    touchTooltipData: BarTouchTooltipData(
+      tooltipBgColor: Colors.transparent,
+      tooltipPadding: const EdgeInsets.all(0),
+      tooltipMargin: 8,
+      getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+          ) {
+        return BarTooltipItem(
+          rod.y.round().toString(),
+          const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+    ),
+  );
+
+  FlTitlesData get titlesData => FlTitlesData(
+    show: true,
+    bottomTitles: SideTitles(
+      showTitles: true,
+      getTextStyles: (context, value) => const TextStyle(
+        color: Color(0xff7589a2),
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+      margin: 20,
+      getTitles: (double value) {
+        switch (value.toInt()) {
+          case 0:
+            return '1*';
+          case 1:
+            return '2*';
+          case 2:
+            return '3*';
+          case 3:
+            return '4*';
+          case 4:
+            return '5*';
+          default:
+            return '';
+        }
+      },
+    ),
+    leftTitles: SideTitles(showTitles: false),
+    topTitles: SideTitles(showTitles: false),
+    rightTitles: SideTitles(showTitles: false),
+  );
+
+  FlBorderData get borderData => FlBorderData(
+    show: false,
+  );
+
+  List<BarChartGroupData> get barGroups => [
+    BarChartGroupData(
+      x: 0,
+      barRods: [
+        BarChartRodData(
+            width:20,y: 8, colors: [Colors.grey])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 1,
+      barRods: [
+        BarChartRodData(
+            width:20,y: 10, colors: [Colors.grey])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 2,
+      barRods: [
+        BarChartRodData(
+            width:20,y: 15, colors: [Colors.grey])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(
+            width:20,y: 25, colors: [Colors.pinkAccent])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 4,
+      barRods: [
+        BarChartRodData(
+            width:20,y: 18, colors: [Colors.grey])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+  ];
+}
