5d079ac567bb634c3c3b6c4041f8357ed9984471
rosha <roshanmathewsofficial@gmail.com>
2022-01-15 14:45:26 +0530

    feat: Initialize mediaquery

---
+++ lib/Widgets/Constants/size.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static getHeight(context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static getWidth(context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static getSize(context) {
+    return MediaQuery.of(context).size;
+  }
+}
